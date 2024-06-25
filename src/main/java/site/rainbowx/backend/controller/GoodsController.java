754f487ee11e11d353ae23556729c3f79bca7382
RainbowX <yan3260325524@outlook.com>
2024-06-25 16:21:40 +0800

    添加商品逻辑

--- src/main/java/site/rainbowx/backend/controller/GoodsController.java
+++ src/main/java/site/rainbowx/backend/controller/GoodsController.java
@@ -2,11 +2,13 @@ package site.rainbowx.backend.controller;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import site.rainbowx.backend.entity.Goods;
 import site.rainbowx.backend.service.GoodsService;
+import site.rainbowx.backend.utils.TokenUtils;
 
 
 import java.util.List;
@@ -16,8 +18,30 @@ public class GoodsController {
     private static final Logger logger = LoggerFactory.getLogger(GoodsController.class);
     @Autowired
     private GoodsService goodsService;
+
+    public static class AddArg{
+        public String token;
+        public String name;
+        public int price;
+        public String thumbnail;
+        public String description;
+    }
+
     @RequestMapping(value = "/get", method = RequestMethod.GET)
     public List<Goods> getAllGoods() {
         return goodsService.getAllGoods();
+    }
+
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    public boolean addGoods(@RequestBody AddArg addArg) {
+        String username = TokenUtils.ValidateToken(addArg.token);
+        if(username == null){ return false; }
+
+        Goods goods = new Goods();
+        goods.setName(addArg.name);
+        goods.setPrice(addArg.price);
+        goods.setThumbnail(addArg.thumbnail);
+        goods.setDescription(addArg.description);
+        goodsService.saveGoods()
     }
 }
