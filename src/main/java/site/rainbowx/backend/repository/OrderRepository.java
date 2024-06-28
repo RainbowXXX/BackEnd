f65d56afd02a44c59643183e3a3bca31f8eb1eb8
feifei <2407828344@qq.com>
2024-06-28 15:10:19 +0800

    bug fix: Fail to load OrderRepository

--- src/main/java/site/rainbowx/backend/repository/OrderRepository.java
+++ src/main/java/site/rainbowx/backend/repository/OrderRepository.java
@@ -1,6 +1,10 @@
 package site.rainbowx.backend.repository;
 
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import site.rainbowx.backend.entity.Orders;
 import site.rainbowx.backend.entity.User;
 
@@ -9,5 +13,8 @@ import java.util.List;
 public interface OrderRepository extends JpaRepository<Orders, Long> {
     List<Orders> findByUser(User user);
 
-    void updateById(Long id, boolean is_paid);
+    @Modifying
+    @Transactional
+    @Query("UPDATE Orders o SET o.is_paid = :is_paid WHERE o.id = :id")
+    void updateById(@Param("id")Long id, @Param("is_paid")Boolean is_paid);
 }
